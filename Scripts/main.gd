d0a28d9b808eeeaa9df8272127381d831e43387d
SPETSNAZ <xofspetnaz@gmail.com>
2025-03-20 16:29:54 +0530

    menu anim change

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -4,17 +4,31 @@ extends CanvasGroup
 @onready var animation_player: AnimationPlayer = $CanvasLayer/AnimationPlayer
 
 func _ready() -> void:
-	audio.play()
-	animation_player.play("new_animation")
-	
+	animation_player.play("anim_0")
+	$Timer.start()
 func _on_touch_screen_button_pressed() -> void:
 	var game = preload("res://Scenes/Levels/world.tscn").instantiate()
 	get_tree().get_root().add_child(game)
 	get_tree().get_root().remove_child(self)
+
+func random_anim():
+	var rand = randi_range(0,3)
+	$Timer.start()
+	if rand == 0 :
+		animation_player.stop()
+		animation_player.play("anim_1")
+	elif rand == 1 :
+		animation_player.stop()
+		animation_player.play("anim_2")
+	elif rand == 2 :
+		animation_player.stop()
+		animation_player.play("anim_3")
+	elif rand == 3 :
+		animation_player.stop()
+		animation_player.play("anim_4")
 	
 
-func _process(_delta: float) -> void:
-	if cam.zoom != Vector2(1,1):
-		cam.zoom = cam.zoom.lerp(Vector2(1,1), 0.001)
-	
+
+func _on_timer_timeout() -> void:
+	random_anim()
 	
